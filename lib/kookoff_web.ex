33be5cb65b071c5441ab0fafd41852c1f5abbceb
Michael Miller-Hairston <michael.millerhairston@gmail.com>
2024-10-02 21:40:08 -0700

    Hide controllers (not sure if I need it)

--- lib/kookoff_web.ex
+++ lib/kookoff_web.ex
@@ -17,17 +17,17 @@ defmodule KookoffWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  # def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
-  def router do
-    quote do
-      use Phoenix.Router, helpers: false
+  # def router do
+  #   quote do
+  #     use Phoenix.Router, helpers: false
 
-      # Import common connection and controller functions to use in pipelines
-      import Plug.Conn
-      import Phoenix.Controller
-    end
-  end
+  #     # Import common connection and controller functions to use in pipelines
+  #     import Plug.Conn
+  #     import Phoenix.Controller
+  #   end
+  # end
 
   def channel do
     quote do
@@ -35,26 +35,26 @@ defmodule KookoffWeb do
     end
   end
 
-  def controller do
-    quote do
-      use Phoenix.Controller,
-        formats: [:html, :json],
-        layouts: [html: KookoffWeb.Layouts]
+  # def controller do
+  #   quote do
+  #     use Phoenix.Controller,
+  #       formats: [:html, :json],
+  #       layouts: [html: KookoffWeb.Layouts]
 
-      import Plug.Conn
+  #     import Plug.Conn
 
-      unquote(verified_routes())
-    end
-  end
+  #     unquote(verified_routes())
+  #   end
+  # end
 
-  def verified_routes do
-    quote do
-      use Phoenix.VerifiedRoutes,
-        endpoint: KookoffWeb.Endpoint,
-        router: KookoffWeb.Router,
-        statics: KookoffWeb.static_paths()
-    end
-  end
+  # def verified_routes do
+  #   quote do
+  #     use Phoenix.VerifiedRoutes,
+  #       endpoint: KookoffWeb.Endpoint,
+  #       router: KookoffWeb.Router,
+  #       statics: KookoffWeb.static_paths()
+  #   end
+  # end
 
   @doc """
   When used, dispatch to the appropriate controller/live_view/etc.
